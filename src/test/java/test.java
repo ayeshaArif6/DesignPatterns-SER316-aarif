705e2e8a164c61a5c786bf6e4932e9b80c0d462d
ayeshaArif6 <[aarif6@asu.edu]>
2024-02-11 22:57:02 -0500

    update test file

---
+++ src/test/java/test.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+import org.junit.Test;
+
+public class test {
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @Test
+    public void testSum(){
+
+        int num1 = 5;
+        int num2 = 7;
+
+        int result = Main.Sum(num1, num2);
+        int expected = 12;
+        assertEquals(expected, result);
+
+    }
+
+}
