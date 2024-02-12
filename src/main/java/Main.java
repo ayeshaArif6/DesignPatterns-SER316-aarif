6d98534a4afb2d8820cefef2576502bf65b6e00d
ayeshaArif6 <[aarif6@asu.edu]>
2024-02-11 22:56:40 -0500

    update main.java

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main{
+	public static void main(String[] args) 
+	{
+		Sum(5,6);
+	}
+
+	public static int Sum(int num1, int num2){
+
+		int add = num1 + num2;
+		System.out.println(add);
+
+		return add;
+	}
+}
